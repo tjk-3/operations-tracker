0eb2f766ac7f6963fc8dbbe0e6498d10a6a66cd0
Thomas J Kenney III <tk3.linkedin@gmail.com>
2026-04-30 21:49:50 -0400

    add main in

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
-from db import create_projects_table
+from db import create_clients_table, create_projects_table, create_payments_table, insert_client, get_all_clients
 
 def main():
+    create_clients_table()
     create_projects_table()
-    print("Project table created succeffully")
+    create_payments_table()
+
+    Damian_Ridealgh = (
+        "Damian Ridealgh",
+        "646-872-1972",
+        "damian.p.ridealgh@gmail.com",
+        "Moennich | 79 Thurber"
+    )
+
+    insert_client(Damian_Ridealgh)
+
+    print("Client inserted successfully!")
+
+clients = get_all_clients()
+
+for client in clients:
+    print(client)
 
 if __name__ == "__main__":
     main()
